faa89687710d21ed7ecd845d6e0799d076317f38
Akash Dubey <akashdubey826@gmail.com>
2022-04-15 19:05:06 +0530

    removed deleted files

--- metrics/kendalls_tau.py
+++
@@ -1,6 +0,0 @@
-from scipy import stats
-
-
-def kendall_tau(true_relevance: list, predicted_relevance: list, cutoff: int):
-    tau, _ = stats.kendalltau(true_relevance[:cutoff], predicted_relevance[:cutoff])
-    return tau
--- metrics/mrr.py
+++
@@ -1,1 +0,0 @@
-def mrr(true_relevance: list, predicted_relevance: list, cutoff: int):
