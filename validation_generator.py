05324b94f08521c0a41b7696346d00180a2bb8ad
Akash Dubey <akashdubey826@gmail.com>
2022-04-26 21:42:38 +0530

    changed var name

--- validation_generator.py
+++ validation_generator.py
@@ -9,11 +9,11 @@ class ValidationGenerator:
     def prep_relevances(true_relevance_items: list, true_relevance_scores: list,
                         predicted_relevance_items: list):
         normalised_true_relevance = list(zip(true_relevance_items, true_relevance_scores))
-        true_dict = dict(normalised_true_relevance)
+        true_relevance_dict = dict(normalised_true_relevance)
         normalised_predicted_relevance = []  # list of tuples
         for predicted_item in predicted_relevance_items:
-            if predicted_item in true_dict:
-                normalised_predicted_relevance.append((predicted_item, true_dict[predicted_item]))
+            if predicted_item in true_relevance_dict:
+                normalised_predicted_relevance.append((predicted_item, true_relevance_dict[predicted_item]))
             else:
                 normalised_predicted_relevance.append((predicted_item, 0))
         return normalised_true_relevance, normalised_predicted_relevance
