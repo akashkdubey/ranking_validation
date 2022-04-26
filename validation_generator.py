1cc6feee890d8310ef4e611b173a1442d3a37795
Akash Dubey <akashdubey826@gmail.com>
2022-04-26 21:33:23 +0530

    added prep_for_normalisation

--- validation_generator.py
+++ validation_generator.py
@@ -1,7 +1,20 @@
 class ValidationGenerator:
 
-    def __init__(self, true_df=None, prediction_df=None):
-        self.true_df = true_df
-        self.prediction_df = prediction_df
+    def __init__(self, true_relevance_items=None, true_relevance_scores=None, predicted_relevance_items=None):
+        self.true_relevance_items = true_relevance_items
+        self.true_relevance_scores = true_relevance_scores
+        self.predicted_relevance_items = predicted_relevance_items
 
+    @staticmethod
+    def prep_for_normalisation(true_relevance_items: list, true_relevance_scores: list,
+                               predicted_relevance_items: list):
+        normalised_true_relevance = list(zip(true_relevance_items, true_relevance_scores))
+        true_dict = dict(normalised_true_relevance)
+        normalised_predicted_relevance = []  # list of tuples
+        for predicted_item in predicted_relevance_items:
+            if predicted_item in true_dict:
+                normalised_predicted_relevance.append((predicted_item, true_dict[predicted_item]))
+            else:
+                normalised_predicted_relevance.append((predicted_item, 0))
+        return normalised_true_relevance, normalised_predicted_relevance
 
